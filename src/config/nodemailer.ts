56a43f74a9e5d212dc45e22d5b390a118ea24e11
AgustinSegui <agussegui24@hotmail.es>
2024-12-03 21:20:34 -0300

    feat: change modal in the responsive movil

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,16 +1,17 @@
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config()
 
 const config = () => {
     return {
-        host: process.env.SMTP_HOST ,
-        port: +process.env.SMTP_PORT ,
+        host: process.env.SMTP_HOST,
+        port: +process.env.SMTP_PORT,
         auth: {
-            user: process.env.SMTP_USER ,
-            pass: process.env.SMTP_PASS 
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
         }
     }
 }
 
-export const transporter= nodemailer.createTransport(config());
+export const transporter = nodemailer.createTransport(config());
+
